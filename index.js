7dea435dc4af92dd6cd19dd4e5bd4cb46e69b150
Jumi Yoon <jums.code@gmail.com>
2022-10-16 22:34:04 -0700

    commit tree d14a0e8c6585f1b97dc71d8a7ace50af1f197ba1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jumi",
+    streetAddress: "1234 ABC street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newList = {...employee};
+    newList[key] = value;
+    return newList
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newList = {...employee};
+    delete newList[key];
+    return newList
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee[key];
+    return employee
+}
